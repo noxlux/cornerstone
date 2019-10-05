bcc5d769efac52f076fe35ff41638f7d8255906c
Micah Neal <noxlux@users.noreply.github.com>
2019-10-05 00:22:04 -0500

    listener for bundle

--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -13,6 +13,38 @@ export default class Category extends CatalogPage {
             this.onSortBySubmit = this.onSortBySubmit.bind(this);
             hooks.on('sortBy-submitted', this.onSortBySubmit);
         }
+
+        document.getElementById('three_specific_jars_and_three_coffepots').addEventListener('click', this.threeCoffepotsThreeJars());
+    }
+
+    threeCoffepotsThreeJars() {
+        this.postData('/api/storefront/cart', {
+            lineItems: [
+                {
+                    quantity: 3,
+                    productId: 93,
+                    variant_id: 46,
+                },
+                {
+                    quantity: 3,
+                    productId: 86,
+                },
+            ],
+        })
+            .then(data => console.log(JSON.stringify(data)))
+            .catch(error => console.error(error));
+    }
+
+    postData(url = '', cartItems = {}) {
+        return fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cartItems),
+        })
+            .then(response => response.json());
     }
 
     initFacetedSearch() {
